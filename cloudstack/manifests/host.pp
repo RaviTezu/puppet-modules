012ba87aefc132fd78d3c174d97e1e5adf01136e
RaviTeja <rpothan@corp.untd.com>
2014-01-13 17:48:16 +0530

    more config for host manifest

--- cloudstack/manifests/host.pp
+++ cloudstack/manifests/host.pp
@@ -6,6 +6,15 @@ class cloudstack::host {
     package {'qemu-kvm':
         ensure => present,
     }
+    
+    file{'/etc/libvirt/qemu.conf':
+        ensure  => present,
+        content => template('cloudstack/host/qemu_conf.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['qemu-kvm']
+    }
 
     package {'libvirt':
         ensure => present,
@@ -15,6 +24,26 @@ class cloudstack::host {
         ensure  => running,
         enable  => true,
         require => Package['libvirt']
+    }
+    
+    file {'/etc/libvirt/libvirtd.conf':
+        ensure  => present,
+        content => template('cloudstack/host/libvirtd_conf.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['libvirt'],
+        notify  => Service['libvirtd']
+    }
+    
+    file {'/etc/sysconfig/libvirtd':
+        ensure  => present,
+        content => template('cloudstack/host/sys_libvirtd.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['libvirt'],
+        notify  => Service['libvirtd']
     }
    
     package {'cloudstack-agent':
@@ -26,4 +55,4 @@ class cloudstack::host {
         enable  => true,
         require => Package['cloudstack-agent']
     }
-    
+}    
